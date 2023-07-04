60456e6d1307655b2090e09e8b8b217192171501
Rommel C. Ramos <ramosrommel2001@gmail.com>
2023-07-04 15:48:35 +0800

    Episode 1 to Episode 30

--- database/migrations/2023_07_04_015809_create_posts_table.php
+++ database/migrations/2023_07_04_015809_create_posts_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,9 +14,23 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('titles');
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('excerpt');
+            $table->text('body');
             $table->timestamps();
+            $table->timestamp('published_at')->nullable();
         });
+
+        // DB::table('posts')->insert([
+        //     'category_id' => '1',
+        //     'slug' => 'slug',
+        //     'title' => 'World',
+        //     'excerpt' => 'hello',
+        //     'body' => 'Lorem ipsum dolor sit'
+        // ]);
     }
 
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,6 +21,45 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+        // User::truncate();
+        // Category::truncate();
+        // Post::truncate();
+        $user =  User::factory()->create([
+            'name' => 'Rommel Ramos'
+        ]);
+
+        Post::factory()->create([
+            'user_id' => $user->id
+        ]);
+        // $user = User::factory()->create();
+
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family',
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work',
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family Post',
+        //     'slug' => 'my-first-post',
+        //     'excerpt' => 'Lorem ipsum hello world',
+        //     'body' => 'Lorem ipsum hello world'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-second-post',
+        //     'excerpt' => 'Lorem ipsum hello world',
+        //     'body' => 'Lorem ipsum hello world'
         // ]);
     }
 }
