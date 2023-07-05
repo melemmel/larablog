1fb71aaec51703d5757268ecad94d97a5b26d07f
Rommel C. Ramos <ramosrommel2001@gmail.com>
2023-07-05 14:21:08 +0800

    Episode 35 How to Extract a Dropdown Blade Component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @include('_post-header')
+    @include('_posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
             <x-post-featured-card :post="$posts[0]" />
