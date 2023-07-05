abde0995757c4c8bc1d0aeebc7dfae733f697cbe
Rommel C. Ramos <ramosrommel2001@gmail.com>
2023-07-05 11:45:11 +0800

    Episode 34 A Small JavaScript Dropdown Detour

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,6 @@
     @include('_post-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
-
             <x-post-featured-card :post="$posts[0]" />
             @if ($posts->count() > 1)
                 <x-posts-grid :post="$posts" />
