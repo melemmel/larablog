1a8728489a8b0bb7ef2eef96d3c8109dea188438
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-08 21:19:20 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/register',[RegisterController::class, 'create'])->middleware('guest');
-Route::post('/register',[RegisterController::class, 'store'])->middleware('guest');;
-Route::post('/logout',[SessionsController::class, 'destroy']);
+Route::get('register',[RegisterController::class, 'create'])->middleware('guest');
+Route::post('register',[RegisterController::class, 'store'])->middleware('guest');;
+
+Route::post('logout',[SessionsController::class, 'destroy']);
