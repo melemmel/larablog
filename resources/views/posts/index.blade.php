1b59dd2046d1b31d8ad5ef7ccae97886056ba4cb
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-08 18:51:01 +0800

    Episode 44 Laughably Simple Pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,10 +2,8 @@
     @include('posts._header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
-            <x-post-featured-card :post="$posts[0]" />
-            @if ($posts->count() > 1)
-                <x-posts-grid :posts="$posts" />
-            @endif
+            <x-posts-grid :posts="$posts" />
+            {{ $posts->links() }}
         @else
             <p class="text-center">No post yet. Please check back later</p>
         @endif
