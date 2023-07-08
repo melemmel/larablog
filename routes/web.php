d5429f88ba8fdf2238828ae1f9e9328a2be902bd
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-08 21:42:15 +0800

    Episode 50 Build the Log In Page

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,6 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('register',[RegisterController::class, 'create'])->middleware('guest');
 Route::post('register',[RegisterController::class, 'store'])->middleware('guest');;
 
-Route::post('logout',[SessionsController::class, 'destroy']);
+Route::get('login',[SessionsController::class, 'create'])->middleware('guest');
+Route::post('login',[SessionsController::class, 'store'])->middleware('guest');
+Route::post('logout',[SessionsController::class, 'destroy'])->middleware('auth');
