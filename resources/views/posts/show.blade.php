0e89fcf2d48d8a0a12e13052ccc712d51b83d820
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-09 10:33:48 +0800

    Episode 57 Some Light Chapter Clean Up

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -49,35 +49,8 @@
             </div>
 
             <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                <x-panel>
-                    @auth
-                        <form action="/posts/{{ $post->slug }}/comments" method="POST" class="">
-                            @csrf
-                            <header class="flext items-center">
-                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60"
-                                    height="60" class="rounded-xl">
-
-                                <h2>Want to participate?</h2>
-                            </header>
-
-                            <div class="mt-6">
-                                <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" rows="5"
-                                    placeholder="Quick, thing of something to say!" required></textarea>
-
-                            </div>
-
-                            <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                                <button type="submit"
-                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                            </div>
-
-                        </form>
-                    @else
-                        <p class="font-semibold">
-                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Login</a>  to leave a comment.
-                        </p>
-                    @endauth
-                </x-panel>
+                @include('posts._add-comment-form')
+                
                 @foreach ($post->comments as $comment)
                     <x-post-comment :comment="$comment" />
                 @endforeach
