39bbb07332dcae6d88b774b0b127f83fa622fbe5
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-09 12:03:52 +0800

    Episode 58 Mailchimp API Tinkering

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,22 @@
 
 <script src="//unpkg.com/alpinejs" defer></script>
 
+<style>
+
+    html{
+        scroll-behavior: smooth;
+    }
+    .clamp {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+
+    .clamp .one-line {
+        -webkit-line-clamp: 1;
+    }
+</style>
+
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
@@ -32,7 +48,7 @@
                 @endauth
 
 
-                <a href="#"
+                <a href="#newsletter"
                     class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
                 </a>
@@ -41,7 +57,9 @@
 
 
         {{ $slot }}
-        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+
+
+        <footer id = "newsletter" class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('ping', function () {
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us13'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('cf1546d95b',[
+        'email_address' => 'ramosrommel2001@gmail.com',
+        'status' => 'subscribed'
+    ]);
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::post('posts/{post:slug}/comments',[PostCommentsController::class, 'store']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
-Route::get('register',[RegisterController::class, 'create'])->middleware('guest');
-Route::post('register',[RegisterController::class, 'store'])->middleware('guest');;
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');;
 
-Route::get('login',[SessionsController::class, 'create'])->middleware('guest');
-Route::post('login',[SessionsController::class, 'store'])->middleware('guest');
-Route::post('logout',[SessionsController::class, 'destroy'])->middleware('auth');
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
