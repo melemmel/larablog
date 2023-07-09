532c53c80dece96e2e47ff2a56cdb8a74af21847
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-09 09:32:40 +0800

    Episode 54 Make the Comments Section Dynamic

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,6 +20,9 @@ class CommentFactory extends Factory
     {
         return [
             //
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
