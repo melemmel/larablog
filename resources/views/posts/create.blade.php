5f522323b553648f9a267480ff59fe0d963ea933
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-11 08:28:00 +0800

    Episode 65 Extract Form-Specific Blade Components

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,19 +4,8 @@
         <x-panel>
             <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="title">
-                        Title
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="title" id="title"
-                        value="{{ old('title') }} "required>
-
-                    @error('title')
-                        <p class="text-red-500
-                        text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
+                </x-form.input name="title"/>
+                
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="slug">
                         Slug
