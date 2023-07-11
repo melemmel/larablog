1958f7ed4956cde93ce87fe7e0014871e0e9ad11
Rommel Ramos <ramosrommel2001@gmail.com>
2023-07-11 14:15:24 +0800

    Episode 69 All About Authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,5 +25,13 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Model::unguard();
+
+        Gate::define('admin', function (User $user) {
+            return $user->username == 'Rommel';
+        });
+
+        Blade::if('admin', function () {
+            return request()->user()->can('admin');
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -38,11 +38,15 @@ Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
-// Admin
-Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
 
-Route::get('admin/posts/', [AdminPostController::class, 'index'])->middleware('admin');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
-Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
-Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
+// Admin
+
+Route::middleware('can:admin')->group(function () {
+    // Route::resource('admin/posts', AdminPostController::class)->excerpt('show);
+    Route::post('admin/posts', [AdminPostController::class, 'store']);
+    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+    Route::get('admin/posts/', [AdminPostController::class, 'index']);
+    Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+    Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
+    Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
+});
